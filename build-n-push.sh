1713d87eda1ec74ea0ff463ba45d3ec28132aa59
David Volm <david@volminator.com>
2022-11-05 11:39:00 -0500

    modified:   build-n-push.sh

--- build-n-push.sh
+++ build-n-push.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 docker build . -t docker.io/daxxog/jbcleanupcode:$(date +%F) \
+    && docker tag docker.io/daxxog/jbcleanupcode:$(date +%F) \
+                  docker.io/daxxog/jbcleanupcode:latest \
     && docker push docker.io/daxxog/jbcleanupcode:$(date +%F) \
+    && docker push docker.io/daxxog/jbcleanupcode:latest \
 ;
 #    && docker run -i -t docker.io/daxxog/jbcleanupcode:$(date +%F)
